af3f7ba946f5410c6b5fa472209915dbbb8598fd
silkskier <121263412+silkskier@users.noreply.github.com>
2023-02-09 03:19:09 +0100

    Source files used for compilation alpha 1.0.0 release

--- release_source/main.cpp
+++ release_source/main.cpp
@@ -11,11 +11,15 @@
 
 int main(int argc, char *argv[]) {
 if (argc < 2) {
-std::cout << "Error: Please specify an option." << std::endl;
-std::cout << "Usage: " << argv[0] << " [OPTION]" << std::endl;
-std::cout << " -b, --batch Run main_batch function." << std::endl;
-std::cout << " -p, --peaks Run main_peaks function." << std::endl;
-std::cout << " -s, --spectrum Run main_spectrum function." << std::endl;
+		printf("\n Generalised Lomb-Scargle periodogram for variable stars, version beta 1.0.\n");
+		printf(" Based on GLS algorithm by Mathias Zechmeister (https://github.com/mzechmeister/GLS).\n");
+		printf(" GitHub page of the project: https://github.com/silkskier/glspeaks \n\n");
+std::cout << "\n No mode specified." << std::endl;
+std::cout << " Usage: " << argv[0] << " [OPTION] \n" << std::endl;
+std::cout << "\t-s, --spectrum \t Writes GLS power spectrum into tsv file." << std::endl;
+std::cout << "\t-p, --peaks \t Prints 20 best-fitting periods of brightness change for the specified data." << std::endl;
+std::cout << "\t-b, --batch \t Calculates best-fitting periods of brightness change for all the data files in specified directory and write the filtered results into tsv file.\n" << std::endl;
+std::cout << "\t-h, --help \t Prints help page (WIP).\n" << std::endl;
 return 1;
 }
 
@@ -24,12 +28,9 @@ std::string option(argv[1]);
 				if (option == "-s" || option == "--spectrum") {
 
 		if(argc < 6){
-     printf("\n Generalized Lomb-Scargle periodogram for variable stars, version of 2022\n");
-     printf(" based on C version of GLS by Mathias Zechmeister (https://github.com/mzechmeister/GLS).\n");
-     printf(" Computing of amplitude spectrum for time series data.\n\n");
-     printf(" Usage: GLS <Path to file> <Min frequency> <Max frequency> <Resolution>\n\n"); return 1;}
+	std::cout << "\n Usage: " << argv[0] <<" "<< argv[1] << " <Path to input file> <Min frequency> <Max frequency> <Resolution>\n" << std::endl; return 1;}
 
-if (std::stoi(argv[5]) > 23) {printf(" Error: Step sizes smaller than 2^-23 unsupported due to limitations of 32-bit floats"); return 1;}
+	if (std::stoi(argv[5]) > 23) {printf(" Error: Step sizes smaller than 2^-23 unsupported due to the limitations of 32-bit floats"); return 1;}
 
 
 
@@ -48,12 +49,9 @@ main_spectrum(argc, argv); return 0;}
 				 else if (option == "-p" || option == "--peaks") {
 
 	if(argc < 6){
-     printf("\n Generalized Lomb-Scargle periodogram for variable stars, version of 2022\n");
-     printf(" based on C version of GLS by Mathias Zechmeister (https://github.com/mzechmeister/GLS).\n");
-     printf(" Computing of amplitude spectrum for time series data.\n\n");
-     printf(" Usage: GLS <Path to file> <Min frequency> <Max frequency> <Resolution>\n\n"); return 1;}
+	std::cout << "\n Usage: " << argv[0] <<" "<< argv[1] << " <Path to input file> <Min frequency> <Max frequency> <Resolution>\n" << std::endl; return 1;}
 
-if (std::stoi(argv[5]) > 23) {printf(" Error: Step sizes smaller than 2^-23 unsupported due to limitations of 32-bit floats"); return 1;}
+	if (std::stoi(argv[5]) > 23) {printf(" Error: Step sizes smaller than 2^-23 unsupported due to the limitations of 32-bit floats"); return 1;}
 
 
 
@@ -68,14 +66,22 @@ main_peaks(argc, argv);return 0;}
 				 else if (option == "-b" || option == "--batch") {
 
 	if(argc < 10){
-		printf("\n Batch GLS period finder for variable stars, version 1.0\n");
-		printf(" based on GLS algorithm by Mathias Zechmeister (https://github.com/mzechmeister/GLS)\n\n");
-		std::cout << " Usage: " << argv[0] << " " << argv[1] << " <Path to catalog with data> <Min frequency> <Max frequency> <Resolution> <Max/Avg> <Filter range> <Min amplitude> <Max amplitude>\n" <<std::endl; return 1;}
+		std::cout << "\n Usage: " << argv[0] << " " << argv[1] << " <Path to catalog with input data files> <Min frequency> <Max frequency> <Resolution> <Max/Avg> <Frequency filter range> <Min amplitude> <Max amplitude>\n" <<std::endl; return 1;}
 
-if (std::stoi(argv[5]) > 23) {printf(" Error: Step sizes smaller than 2^-23 unsupported due to limitations of 32-bit floats"); return 1;}
+if (std::stoi(argv[5]) > 23) {printf(" Error: Step sizes smaller than 2^-23 unsupported due to the limitations of 32-bit floats"); return 1;}
 if(stof(argv[3]) <= 0){printf("\n Error; Starting frequency must be greater, than 0\n"); return 1;}
 
+
 main_batch(argc, argv);return 0;}
+
+
+
+
+				 else if (option == "-h" || option == "--help") {
+
+std::cout << " Help page not available in release alpha 1.0.\n Planned as addition to full 1.0 release." <<std::endl;
+ return 0;}
+
 
  else {
 std::cout << "Error: Invalid mode selected." << std::endl;
