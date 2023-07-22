2b6638eeb98a235ddc9351bb05e80b274299a4d2
silkskier <121263412+silkskier@users.noreply.github.com>
2023-07-22 15:51:12 +0200

    Add plotting extention (WIP)

---
+++ plot/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <filename.tsv>")
+		return}
+
+	fileName := os.Args[1]
+	var data [][]string
+
+	// Open the file for reading.
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+
+	scanner := bufio.NewScanner(file)
+
+	// Read each line and split it into fields based on tabs.
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Split(line, "\t")
+		data = append(data, fields)}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return}
+
+	// Print the array.
+		fmt.Println(data)
+		//fmt.Println(data[3][3])
+}
