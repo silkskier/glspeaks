5a1b93df2fb19e3b2a8a2507752aedb158df573d
silkskier <121263412+silkskier@users.noreply.github.com>
2023-07-23 19:02:28 +0200

    Add files via upload

--- plot/main.go
+++ plot/main.go
@@ -9,6 +9,7 @@ import (
 	"sort"
 	"path"
 	"strconv"
+	//"sync"
 
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
@@ -53,6 +54,101 @@ func (customXTicks) Ticks(min, max float64) [] plot.Tick {
 	return ticks
 }
 
+func generatePlot(file string, outputDir string, frequency float64, match_strength string, photometry_location string){
+	plotname := outputDir + "/" + match_strength + "_" + file + ".png"
+
+	var err error
+
+	var local_data [][]float64
+	loc_file, loc_err := os.Open(photometry_location)
+	if loc_err != nil {
+		fmt.Println("Error opening file:", loc_err)
+		return}
+	defer loc_file.Close()
+
+	local_scanner := bufio.NewScanner(loc_file)
+	// Read each line and split it into fields based on spaces.
+		for local_scanner.Scan() {
+		line := local_scanner.Text()
+		local_fields := strings.Split(line, " ")
+
+		// Convert the string fields to float64 values.
+		var floatFields [] float64
+		for _, field := range local_fields {
+			val, err := strconv.ParseFloat(field, 64)
+			if err != nil {
+			fmt.Println("Error parsing float:", err)
+			return
+			}
+			floatFields = append(floatFields, val)
+		}
+
+		local_data = append(local_data, floatFields)
+	}
+
+		if err := local_scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+		// Print the array	for _, line := range local_data {fmt.Println(line)}
+	x := make([]float64, len(local_data))
+	y := make([]float64, len(local_data))
+		for i, line := range local_data {
+			x[i] = line[0]
+			y[i] = line[1]
+	}
+
+		for i := range x {x[i] = math.Mod(x[i]*frequency, 2.0)}
+		for i := range y {y[i] = -y[i]}
+
+
+		//Plot the measurements
+		plot_data := make(plotter.XYs, len(local_data))
+	for i := range local_data {
+		plot_data[i].X = x[i]
+		plot_data[i].Y = y[i]
+	}
+
+	plt := plot.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the scatter plot style to use filled circles.
+	s, err := plotter.NewScatter(plot_data)
+	if err != nil {
+		panic(err)
+	}
+	s.GlyphStyle.Shape = draw.CircleGlyph{}
+	s.GlyphStyle.Color = plotutil.DefaultColors[0]
+
+	plt.X.Min = 0
+	plt.X.Max = 2
+
+	plt.Add(s)
+
+	// Customize the tick labels with a custom formatter
+	plt.Y.Tick.Marker = customYTicks{}
+	plt.X.Tick.Marker = customXTicks{}
+
+
+	// Set the plot size in pixels (width x height).
+	widthPixels, heightPixels := 600, 300
+
+	// Save the plot to a file with the specified size in pixels.
+	err = plt.Save(vg.Length(widthPixels), vg.Length(heightPixels), plotname)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+
+
+
+
+
 
 func main() {
 	if len(os.Args) < 2 {
@@ -96,6 +192,8 @@ func main() {
 		return
 	}
 
+	data = data[1:]
+
 
 	for _, file := range data {
 		file[4] = strings.ReplaceAll(file[4], ".", "")
@@ -114,107 +212,15 @@ func main() {
 
 
 
-
-
-
-	for _, file := range data {	//main loop
-	filename := strings.TrimSuffix(path.Base(file[0]), path.Ext(file[0]))
-
-	frequency, err := strconv.ParseFloat(file[1], 64)
-		if err != nil {panic(err)}
-
-	plotname := plotsDir + "/" + file[4] + "_" + filename + ".png"
-	//fmt.Println(plotname)
-
-	photometry_location := strings.Trim(file[0], `"`)
-
-	var local_data [][]float64
-	// Open the file for reading.
-	loc_file, loc_err := os.Open(photometry_location)
-	if loc_err != nil {
-		fmt.Println("Error opening file:", loc_err)
-		return
-	}
-	defer loc_file.Close()
-
-	local_scanner := bufio.NewScanner(loc_file)
-	// Read each line and split it into fields based on spaces.
-	for local_scanner.Scan() {
-		line := local_scanner.Text()
-		local_fields := strings.Split(line, " ")
-
-		// Convert the string fields to float64 values.
-		var floatFields [] float64
-		for _, field := range local_fields {
-			val, err := strconv.ParseFloat(field, 64)
+	for _, file := range data {  //call a function here
+		freq, err := strconv.ParseFloat(file[1], 64)
 			if err != nil {
-				fmt.Println("Error parsing float:", err)
-				return
-			}
-			floatFields = append(floatFields, val)
-		}
-
-		local_data = append(local_data, floatFields)
-	}
-
-	if err := local_scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-
-	// Print the array	for _, line := range local_data {fmt.Println(line)}
-	x := make([]float64, len(local_data))
-	y := make([]float64, len(local_data))
-		for i, line := range local_data {
-			x[i] = line[0]
-			y[i] = line[1]
-	}
-
-		for i := range x {x[i] = math.Mod(x[i]*frequency, 2.0)}
-		for i := range y {y[i] = -y[i]}
-
-
-		//Plot the measurements
-		plot_data := make(plotter.XYs, len(local_data))
-	for i := range local_data {
-		plot_data[i].X = x[i]
-		plot_data[i].Y = y[i]
-	}
-
-	plt := plot.New()
-	if err != nil {
-		panic(err)
-	}
-
-	// Set the scatter plot style to use filled circles.
-	s, err := plotter.NewScatter(plot_data)
-	if err != nil {
-		panic(err)
-	}
-	s.GlyphStyle.Shape = draw.CircleGlyph{}
-	s.GlyphStyle.Color = plotutil.DefaultColors[0]
-
-	plt.X.Min = 0
-	plt.X.Max = 2
-	//plt.Y.Inverted = true
-
-
-
-	plt.Add(s)
-
-	// Customize the tick labels with a custom formatter
-	plt.Y.Tick.Marker = customYTicks{}
-	plt.X.Tick.Marker = customXTicks{}
-
-
-	// Set the plot size in pixels (width x height).
-	widthPixels, heightPixels := 600, 300
-
-	// Save the plot to a file with the specified size in pixels.
-	err = plt.Save(vg.Length(widthPixels), vg.Length(heightPixels), plotname)
-	if err != nil {
-		panic(err)
+			fmt.Println("Error parsing float:", err)
+			return}
+		generatePlot(strings.TrimSuffix(path.Base(file[0]), path.Ext(file[0])), plotsDir, freq, file[4], strings.Trim(file[0], `"`))
 	}
 
 
-}}
+
+
+}
