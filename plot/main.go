cacb02f13da8a50affb7de2eb5e6d18c68a1a13b
silkskier <121263412+silkskier@users.noreply.github.com>
2023-07-23 02:42:36 +0200

    Add files via upload

--- plot/main.go
+++ plot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"math"
 	"fmt"
 	"os"
 	"strings"
@@ -15,6 +16,26 @@ import (
 	"gonum.org/v1/plot/vg"
 	"gonum.org/v1/plot/vg/draw"
 )
+
+// customYTicks is a custom tick formatter for the Y-axis
+type customYTicks struct{}
+
+// Ticks implements the plot.Ticker interface
+func (customYTicks) Ticks(min, max float64) []plot.Tick {
+	ticks := make([]plot.Tick, 0)
+
+	// Calculate the desired tick spacing based on the range of Y-axis values
+	rangeY := max - min
+	tickSpacing := rangeY / 7.02 // Four ticks between min and max
+
+	for v := min; v <= max; v += tickSpacing {
+		label := fmt.Sprintf("%.2f", math.Abs(v))
+		tick := plot.Tick{Value: v, Label: label}
+		ticks = append(ticks, tick)
+	}
+	return ticks
+}
+
 
 func main() {
 	if len(os.Args) < 2 {
@@ -60,8 +81,8 @@ func main() {
 
 	for _, file := range data {
 		file[4] = strings.ReplaceAll(file[4], ".", "")
-		if len(file[4]) > 4 {
-			file[4] = "9999"
+		if len(file[4]) > 5 {
+			file[4] = "99999"
 		}
 	}
 
@@ -71,9 +92,7 @@ func main() {
 
 	// Print the array.
 	// fmt.Println(data)
-	for _, file := range data {
-		fmt.Println(file)
-	}
+	for _, file := range data {fmt.Println(file)}
 
 
 
@@ -82,7 +101,9 @@ func main() {
 
 	for _, file := range data {	//main loop
 	filename := strings.TrimSuffix(path.Base(file[0]), path.Ext(file[0]))
-	//frequency := file[1]
+
+	frequency, err := strconv.ParseFloat(file[1], 64)
+		if err != nil {panic(err)}
 
 	plotname := plotsDir + "/" + file[4] + "_" + filename + ".png"
 	//fmt.Println(plotname)
@@ -105,7 +126,7 @@ func main() {
 		local_fields := strings.Split(line, " ")
 
 		// Convert the string fields to float64 values.
-		var floatFields []float64
+		var floatFields [] float64
 		for _, field := range local_fields {
 			val, err := strconv.ParseFloat(field, 64)
 			if err != nil {
@@ -124,14 +145,22 @@ func main() {
 	}
 
 	// Print the array	for _, line := range local_data {fmt.Println(line)}
+	x := make([]float64, len(local_data))
+	y := make([]float64, len(local_data))
+		for i, line := range local_data {
+			x[i] = line[0]
+			y[i] = line[1]
+	}
 
+		for i := range x {x[i] = math.Mod(x[i]*frequency, 2.0)}
+		for i := range y {y[i] = -y[i]}
 
 
 		//Plot the measurements
 		plot_data := make(plotter.XYs, len(local_data))
-	for i, line := range local_data {
-		plot_data[i].X = line[0]
-		plot_data[i].Y = line[1]
+	for i := range local_data {
+		plot_data[i].X = x[i]
+		plot_data[i].Y = y[i]
 	}
 
 	plt := plot.New()
@@ -147,7 +176,18 @@ func main() {
 	s.GlyphStyle.Shape = draw.CircleGlyph{}
 	s.GlyphStyle.Color = plotutil.DefaultColors[0]
 
+	plt.X.Min = 0
+	plt.X.Max = 2
+	//plt.Y.Inverted = true
+
+
+
 	plt.Add(s)
+
+	// Customize the Y-axis tick labels with a custom formatter
+	plt.Y.Tick.Marker = customYTicks{}
+
+
 
 	// Set the plot size in pixels (width x height).
 	widthPixels, heightPixels := 600, 300
@@ -157,6 +197,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+
 
 
 }}
