a8bf217c0d7278a42d8481e8e8523f6868897f0a
silkskier <121263412+silkskier@users.noreply.github.com>
2023-07-29 03:05:38 +0200

    Add files via upload

--- train/source/main.go
+++ train/source/main.go
@@ -1,11 +1,12 @@
+
 package main
 
 import (
     "fmt"
 
     "github.com/sjwhitworth/golearn/base"
+    "github.com/sjwhitworth/golearn/ensemble"
     "github.com/sjwhitworth/golearn/evaluation"
-    "github.com/sjwhitworth/golearn/knn"
 )
 
 func main() {
@@ -20,20 +21,22 @@ func main() {
     // Print a pleasant summary of your data.
     fmt.Println(rawData)
 
-    //Initialises a new KNN classifier
-    cls := knn.NewKnnClassifier("euclidean", "linear", 2)
+    // Initialize a new Random Forest classifier
+    forest := ensemble.NewRandomForest(100, 4) // Using 100 trees and 4 features per split
 
-    //Do a training-test split
+    // Do a training-test split
     trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
-    cls.Fit(trainData)
 
-    //Calculates the Euclidean distance and returns the most popular label
-    predictions, err := cls.Predict(testData)
+    // Fit the Random Forest classifier
+    forest.Fit(trainData)
+
+    // Make predictions using the Random Forest classifier
+    predictions, err := forest.Predict(testData)
     if err != nil {
         panic(err)
     }
 
-    // Prints precision/recall metrics
+    // Print precision/recall metrics
     confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
     if err != nil {
         panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
