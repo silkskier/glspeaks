2417a1a6c9e77234ef53afd9f9c652a46cf1c128
silkskier <121263412+silkskier@users.noreply.github.com>
2023-08-09 13:45:05 +0200

    Add files via upload

---
+++ source/extras/vertex_64.hpp
@@ -0,0 +1,16 @@
+#ifndef VERTEX_HPP
+#define VERTEX_HPP
+
+#include <iostream>
+
+struct output_data {
+	double power, amplitude, frequency;
+	double sum_of_powers;
+};
+
+struct measurement {
+    double x;
+    double y;
+};
+
+#endif
