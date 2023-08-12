bb5a6ead0f27ce0d9e0648a2bd7b3d2dbfb72315
silkskier <121263412+silkskier@users.noreply.github.com>
2023-08-12 07:43:13 +0200

    Add files via upload

--- source/batch_cpu/main_b.hpp
+++ source/batch_cpu/main_b.hpp
@@ -6,7 +6,6 @@
 #include <tuple>
 #include <thread>
 #include <atomic>
-#include <chrono>
 #include <locale>
 
 #include <QProgressDialog>
@@ -16,42 +15,12 @@
 #include <boost/spirit/include/karma_real.hpp>
 
 #include "periodogram_b.hpp"
+#include "../extras/filter.hpp"
+
 
 using namespace std;
 
-std::atomic<float> progressValue(0);
-std::atomic<int> timeLeft(0), filesComputed(0);
 
-bool filter(float frequency, float power, float min_value, float filter_range, float amplitude, float min_amplitude, float max_amplitude){
-
-    if (power < min_value) return false; //powers filter
-
-    if (amplitude < min_amplitude || amplitude > max_amplitude) return false; //amplitudes filter
-
-    float filter_frequencies[] = {1, 2, 3, 4, 5, 8};
-    unsigned int num_frequencies = sizeof(filter_frequencies) / sizeof(filter_frequencies[0]);
-    if (frequency == 0) return 0;
-    for (unsigned int i = 0; i < num_frequencies; ++i)
-    {if (frequency > filter_frequencies[i] - (filter_range / 2) && frequency < filter_frequencies[i] + (filter_range / 2)) return false;}//frequencies filter
-
-    return true;
-}
-
-void printProgress(int max_progress, const std::chrono::steady_clock::time_point startTime) {
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        if (filesComputed.load() == max_progress) {
-            return;
-        }
-        int currentFilesComputed = filesComputed.load();
-        float progress = 100. * static_cast<float>(currentFilesComputed) / static_cast<float>(max_progress);
-        std::chrono::duration<double> elapsedTime = std::chrono::steady_clock::now() - startTime;
-        float localTimeLeft = static_cast<float>(elapsedTime.count()) * (float(max_progress) - float(currentFilesComputed)) / float(currentFilesComputed);
-        progressValue.store(10 * progress);
-        timeLeft.store(int(localTimeLeft));
-        std::cout << std::fixed << std::setprecision(2) << "\r" << progress << "% complete, " << localTimeLeft << " seconds left." << std::flush;
-    }
-}
 
 void main_batch(int argc, char *argv[]){
 
