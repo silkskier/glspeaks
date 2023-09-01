63d389b083782bb3d5ef21a7b01a5478f720e89d
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-01 02:23:39 +0200

    Divide gls_b into two functions
    Change made to allow for efficient frequency finetuning in the future. Expected ~ 2% performance increase due to const * const pointers.

--- source/batch_cpu/GLS/GLS_b.hpp
+++ source/batch_cpu/GLS/GLS_b.hpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <iostream>
 #include "../../extras/vertex_32.hpp"
 
 #include "../../extras/NFFT.hpp"
@@ -10,8 +11,69 @@ using namespace std;
 /* Author: Mathias Zechmeister
  * Date: 2018-10-01
  */
-inline output_data gls_b(float* t,float* y,float* e_y, unsigned int n, unsigned int nk, float fstep, float* f) { //
 
+inline void gls_freq(const uint &k, const uint &n,
+      float &D, float &SS, float &YY, float *f,
+      const float* const t, const float* const w, const float* const wy, float* cosx, float* sinx,  const float* const cosdx, const float* const sindx,
+      output_data &best_frequency){
+
+      float C, S, YC, YS, CC, CS, tmp, self_a, self_b;
+      C = S = YC = YS = CC = CS = tmp = self_a = self_b = 0;
+	  float power = 0, amplitude = 0; //declares output variables
+
+      for (uint i=0; i<n; ++i) {
+         if (k % 256 == 0) {
+            /* init/refresh recurrences to stop error propagation */
+            cosx[i] = cos(2 * M_PI * f[k] * t[i]);
+            sinx[i] = sin(2 * M_PI * f[k] * t[i]);
+         }
+
+         C += w[i] * cosx[i];              /* Eq. (8) */
+/*          printf("%.10g %.10g %.10g %.10g %.10g\n", f[k], w[i], t[i], cosx[i], C);*/
+         S += w[i] * sinx[i];              /* Eq. (9) */
+         YC += wy[i] * cosx[i];            /* Eq. (11) */
+         YS += wy[i] * sinx[i];            /* Eq. (12) */
+/*         wcosx = w * cosx; */
+         CC += w[i] * cosx[i] * cosx[i];   /* Eq. (13) */
+         CS += w[i] * cosx[i] * sinx[i];   /* Eq. (15) */
+
+         /* increase freq for next loop */
+         tmp = cosx[i] * cosdx[i] - sinx[i] * sindx[i];
+         sinx[i] = cosx[i] * sindx[i] + sinx[i] * cosdx[i];
+         cosx[i] = tmp;
+
+      }
+
+
+      SS = 1. - CC;
+      CC -= C * C;              /* Eq. (13) */
+      SS -= S * S;              /* Eq. (14) */
+      CS -= C * S;              /* Eq. (15) */
+      D = CC*SS - CS*CS;        /* Eq. (6) */
+
+
+
+      /* power */
+      power = (SS*YC*YC + CC*YS*YS - 2*CS*YC*YS) / (YY*D);  /* Eq. (5) in ZK09 */
+
+      //update output data struct
+if (std::isnormal(power)) {best_frequency.sum_of_powers += power;}
+
+if (power > best_frequency.power){
+    self_a = (YC*SS-YS*CS) / D;
+    self_b = (YS*CC-YC*CS) / D;
+
+    amplitude = sqrt((self_a * self_a) + (self_b * self_b));
+
+  best_frequency.frequency = f[k];
+  best_frequency.amplitude = amplitude;
+  best_frequency.power = power;
+}}
+
+
+
+
+output_data gls_b(float* t,float* y,float* e_y, unsigned int n, unsigned int nk, float fstep, float* f) { //
 output_data best_frequency; best_frequency.power = 0; best_frequency.power = 0; best_frequency.sum_of_powers = 0;
 
    /*
@@ -54,69 +116,17 @@ output_data best_frequency; best_frequency.power = 0; best_frequency.power = 0; 
       cosdx[i] = cos(2 * M_PI * fstep * t[i]);
       sindx[i] = sin(2 * M_PI * fstep * t[i]);
    }
-   /*printf("%i \n");*/
-
-
 
 
 
    for (k=0; k<nk; ++k) {
+         gls_freq(k, n,
+         D, SS, YY,
+         f, t, w, wy, cosx, sinx, cosdx, sindx,
+         best_frequency);
 
-      C = S = YC = YS = CC = CS = 0, self_a = 0, self_b = 0;
-	  float power = 0, amplitude = 0; //declares output variables
-
-      for (i=0; i<n; ++i) {
-         if (k % 256 == 0) {
-            /* init/refresh recurrences to stop error propagation */
-            cosx[i] = cos(2 * M_PI * f[k] * t[i]);
-            sinx[i] = sin(2 * M_PI * f[k] * t[i]);
-         }
-
-         C += w[i] * cosx[i];              /* Eq. (8) */
-/*          printf("%.10g %.10g %.10g %.10g %.10g\n", f[k], w[i], t[i], cosx[i], C);*/
-         S += w[i] * sinx[i];              /* Eq. (9) */
-         YC += wy[i] * cosx[i];            /* Eq. (11) */
-         YS += wy[i] * sinx[i];            /* Eq. (12) */
-/*         wcosx = w * cosx; */
-         CC += w[i] * cosx[i] * cosx[i];   /* Eq. (13) */
-         CS += w[i] * cosx[i] * sinx[i];   /* Eq. (15) */
-
-         /* increase freq for next loop */
-         tmp = cosx[i] * cosdx[i] - sinx[i] * sindx[i];
-         sinx[i] = cosx[i] * sindx[i] + sinx[i] * cosdx[i];
-         cosx[i] = tmp;
-
-      }
-
-
-
-
-
-
-      SS = 1. - CC;
-      CC -= C * C;              /* Eq. (13) */
-      SS -= S * S;              /* Eq. (14) */
-      CS -= C * S;              /* Eq. (15) */
-      D = CC*SS - CS*CS;        /* Eq. (6) */
-
-
-
-      /* power */
-      power = (SS*YC*YC + CC*YS*YS - 2*CS*YC*YS) / (YY*D);  /* Eq. (5) in ZK09 */
-
-      //update output data struct
-if (std::isnormal(power)) {best_frequency.sum_of_powers += power;}
-
-if (power > best_frequency.power){
-    self_a = (YC*SS-YS*CS) / D;
-    self_b = (YS*CC-YC*CS) / D;
-
-    amplitude = sqrt((self_a * self_a) + (self_b * self_b));
-
-  best_frequency.frequency = f[k];
-  best_frequency.amplitude = amplitude;
-  best_frequency.power = power;
-}}
+     }
 
    free(w); free(wy); free(cosx); free(sinx); free(cosdx); free(sindx);
+//std::cout << best_frequency.power << std::endl;
 return best_frequency;}
