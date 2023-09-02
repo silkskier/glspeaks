efe8ce5929550c02e3bcb9d43792003af65ae35f
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-02 03:43:24 +0200

    Add files via upload
    Add bisection in order to enable reaching high precision at low resolution

--- source/batch_cpu/GLS/GLS_b.hpp
+++ source/batch_cpu/GLS/GLS_b.hpp
@@ -8,7 +8,7 @@
  * Date: 2018-10-01
  */
 
-inline void gls_freq(const uint &k, const uint &n,
+inline void gls_freq(const uint &k, const uint &n, //function used for GLS calculation for each frequency
       float &SS, float &YY, float *f,
       const float* const t, const float* const w, const float* const wy, float* cosx, float* sinx,  const float* const cosdx, const float* const sindx,
       output_data &best_frequency){
@@ -62,6 +62,59 @@ if (power > best_frequency.power){
     amplitude = sqrt((self_a * self_a) + (self_b * self_b));
 
   best_frequency.frequency = f[k];
+  best_frequency.amplitude = amplitude;
+  best_frequency.power = power;
+}}
+
+
+
+
+inline void gls_dfreq(const uint &n,  //function used for fine-tuning of resulting best frequency
+      float &YY, double f,
+      const double* const t, const float* const w, const float* const wy,
+      output_data &best_frequency){
+
+      double cosx, sinx, SS, C, D, S, YC, YS, CC, CS, self_a, self_b;
+      C = D = S = YC = YS = CC = CS = self_a = self_b = 0;
+	  float power = 0, amplitude = 0; //declares output variables
+
+	  for (uint i=0; i<n; ++i) {
+
+            /* init/refresh recurrences to stop error propagation */
+            cosx = cos(2 * M_PI * f * t[i]);
+            sinx = sin(2 * M_PI * f * t[i]);
+
+         C += w[i] * cosx;              /* Eq. (8) */
+/*          printf("%.10g %.10g %.10g %.10g %.10g\n", f, w[i], t[i], cosx[i], C);*/
+         S += w[i] * sinx;              /* Eq. (9) */
+         YC += wy[i] * cosx;            /* Eq. (11) */
+         YS += wy[i] * sinx;            /* Eq. (12) */
+/*         wcosx = w * cosx; */
+         CC += w[i] * cosx * cosx;   /* Eq. (13) */
+         CS += w[i] * cosx * sinx;   /* Eq. (15) */
+
+
+      }
+
+
+      SS = 1. - CC;
+      CC -= C * C;              /* Eq. (13) */
+      SS -= S * S;              /* Eq. (14) */
+      CS -= C * S;              /* Eq. (15) */
+      D = CC*SS - CS*CS;        /* Eq. (6) */
+
+
+      /* power */
+      power = (SS*YC*YC + CC*YS*YS - 2*CS*YC*YS) / (YY*D);  /* Eq. (5) in ZK09 */
+
+//update output data struct
+if (power > best_frequency.power){
+    self_a = (YC*SS-YS*CS) / D;
+    self_b = (YS*CC-YC*CS) / D;
+
+    amplitude = sqrt((self_a * self_a) + (self_b * self_b));
+
+  best_frequency.frequency = f;
   best_frequency.amplitude = amplitude;
   best_frequency.power = power;
 }}
@@ -126,6 +179,29 @@ output_data best_frequency; best_frequency.power = 0; best_frequency.power = 0; 
 
      }
 
-   free(w); free(wy); free(cosx); free(sinx); free(cosdx); free(sindx), free(ts);
+   free(cosdx); free(sindx); free(ts); free(cosx); free(sinx);
+   //fine-tune the resulting frequency in order to increase precision
+
+   double dfstep = fstep;
+
+    for (k=0; k<8; ++k) {
+    double f0 = best_frequency.frequency - dfstep;
+    double f1 = best_frequency.frequency + dfstep;
+
+    gls_dfreq(n,
+    YY, f0,
+    t, w, wy,
+    best_frequency);
+
+    gls_dfreq(n,
+    YY, f1,
+    t, w, wy,
+    best_frequency);
+
+    dfstep *= 0.5;
+    }
+
+
+   free(w); free(wy);
 //std::cout << best_frequency.power << std::endl;
 return best_frequency;}
