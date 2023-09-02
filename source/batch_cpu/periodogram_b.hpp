65e15cb9e753e7ff6f8721e4f04f8517c2f8fa73
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-03 01:30:39 +0200

    Add files via upload

--- source/batch_cpu/periodogram_b.hpp
+++ source/batch_cpu/periodogram_b.hpp
@@ -20,7 +20,7 @@ using namespace std;
 
 std::tuple<float, float, float> periodogram(float* frequencies_array, float step_size, int number_of_steps, std::filesystem::path in_file){
 
-star data = read_dat(in_file);
+star data; data.read_dat(in_file);
 
 //unsigned int length_of_data = size(data.x); float t[length_of_data]; float y[length_of_data]; float e_y[length_of_data]; //declares variables used by GLS_periodogram
 unsigned int length_of_data = size(data.x); float *t; float *y; float *e_y; //declares variables used by GLS_periodogram
@@ -30,7 +30,6 @@ output_data best_frequency;
 best_frequency = gls_b(data.x.data(), data.y.data(), data.dy.data(), length_of_data, number_of_steps, step_size, frequencies_array); //declares and fills a powers array //
 
 float powers_average = best_frequency.sum_of_powers / number_of_steps; //calculates average power for the input data
-
 
 std::tuple<double, float, float> output_tuple = make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(length_of_data) ));
 
