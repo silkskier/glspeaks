838314a58948b86a68ffbb02eccb52dcf94a94b1
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-03 19:50:06 +0200

    Add files via upload

--- source/peaks/GLS/GLS_p_par.hpp
+++ source/peaks/GLS/GLS_p_par.hpp
@@ -8,7 +8,7 @@ using namespace std;
 /* Author: Mathias Zechmeister
  * Date: 2018-10-01
  */
-void gls_p_par(double* t,double* y,double* e_y, unsigned int n, unsigned int nk, double fstep,double* f,double* p, double* a) { //
+void gls_p_par(double* t,float* y,float* e_y, unsigned int n, unsigned int nk, double fstep,double* f,double* p, double* a) { //
 
 
    /*
