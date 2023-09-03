f8933424469bbd615c78ff207ecdffd5bdf81d63
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-03 21:46:56 +0200

    Add files via upload

--- source/spectrum/main_s.hpp
+++ source/spectrum/main_s.hpp
@@ -8,8 +8,7 @@
 #include <sstream>
 #include <locale>
 
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/karma.hpp>
+#include "../extras/readout.hpp"
 
 #include "GLS/GLS_s.hpp"
 
@@ -57,67 +56,28 @@ for(unsigned int step=0; step < no_steps;step++){frequencies[step] = min_frequen
 //    for(unsigned int i = 0; i < no_steps ; i++) printf("%f, ", frequencies[i]); // prints frequencies vector
 
 
-std::vector<std::vector<double>> data; std::string line;
-std::ifstream input_file(file);
-
-    if (input_file) {
-        std::string line;
-        while (std::getline(input_file, line)) {
-            std::vector<double> tempVec;
-            std::string::const_iterator it = line.begin();
-            std::string::const_iterator end = line.end();
-
-            bool success = boost::spirit::qi::phrase_parse(it, end, *boost::spirit::qi::double_ >> *(boost::spirit::qi::lit(',') >> boost::spirit::qi::double_), boost::spirit::qi::space, tempVec);
-
-            if (success && it == end) {
-                data.emplace_back(tempVec);
-            } else {
-                std::cout << "Parsing failed" << std::endl;
-                break;
-            }}
-    } else {
-        std::cout << "file cannot be opened" << std::endl;}
-
-    input_file.close();
-//  for(unsigned int i=0; i<data.size(); i++) {for(unsigned int j=0; j<data[i].size(); j++) cout<<data[i][j]<<" "; cout<<endl;} // prints data array
-
+star data;
+data.read(file);
 
 
         //applies Generalized Lomb-Scargle periodogram for all the frequencies
-unsigned int length_of_data = size(data);
-double t[length_of_data]; double y[length_of_data]; double e_y[length_of_data];
-for (unsigned int i = 0; i < length_of_data; i++) t[i] = data[i][0], y[i] = data[i][1], e_y[i] = data[i][2]; //transpose vectors
-data.clear();
-//  for (unsigned int i = 0; i < length_of_data; i++) std::cout<< t[i] <<" "<< y[i] <<" "<< e_y[i] <<std::endl; //prints transposed data array
+unsigned int length_of_data = data.x.size();
 
-gls_s(t, y, e_y, length_of_data, no_steps, step_size_0, frequencies, powers);
+gls_s(data.x.data(), data.y.data(), data.dy.data(), length_of_data, no_steps, step_size_0, frequencies, powers);
 
 //        for (unsigned int i = 0; i < no_steps; i++) std::cout<< frequencies[i] <<" "<< powers[i] <<std::endl; //prints power for each frequency
 // std::cout<< std::filesystem::path(file).filesystem::path::parent_path() <<std::endl; //prints input files directory
-//std::filesystem::path path = std::filesystem::path(file);
-std::filesystem::path output_path = std::filesystem::path (std::filesystem::path(file).string() + "_output.tsv");
+
+std::string output_path = std::filesystem::path(file).string() + "_output.tsv";
 
 
 
+ofstream output_file(output_path); output_file.close(); //creates output file
 
-  std::vector<std::string> output_string(no_steps);
-
-	#pragma omp parallel for
-  for (unsigned int i = 0; i < no_steps; i++) {
-
-  boost::spirit::karma::generate
-	(std::back_inserter(output_string[i]),
-	boost::spirit::double_(frequencies[i])
-	<< "\t"
-	<< boost::spirit::double_(powers[i]));
-}
-
-
-ofstream output_file(output_path); //creates and opens output file
-
+auto out = fmt::output_file(output_path);
 // output_file << "Frequency Power" "\n";
 
-for (unsigned int i = 0; i < no_steps; i++) output_file << output_string[i] << "\n";
+for (unsigned int i = 0; i < no_steps; i++){out.print(fmt::format("{:.5f}\t{:.5f}\n", frequencies[i], powers[i]));}
 
-output_file.close(); return;}
+ return;}
 
