7c819b362d48a63effbf592313ad37578668bea9
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-03 18:19:07 +0200

    Add files via upload

--- source/batch_cpu/periodogram_b.hpp
+++ source/batch_cpu/periodogram_b.hpp
@@ -20,18 +20,18 @@ using namespace std;
 
 std::tuple<float, float, float> periodogram(float* frequencies_array, float step_size, int number_of_steps, std::filesystem::path in_file){
 
-star data; data.read(in_file);
+	star data; data.read(in_file);
 
-//unsigned int length_of_data = size(data.x); float t[length_of_data]; float y[length_of_data]; float e_y[length_of_data]; //declares variables used by GLS_periodogram
-unsigned int length_of_data = size(data.x); float *t; float *y; float *e_y; //declares variables used by GLS_periodogram
+	//unsigned int length_of_data = size(data.x); float t[length_of_data]; float y[length_of_data]; float e_y[length_of_data]; //declares variables used by GLS_periodogram
+	unsigned int length_of_data = size(data.x); float *t; float *y; float *e_y; //declares variables used by GLS_periodogram
 
-output_data best_frequency;
+	output_data best_frequency;
 
-best_frequency = gls_b(data.x.data(), data.y.data(), data.dy.data(), length_of_data, number_of_steps, step_size, frequencies_array); //declares and fills a powers array //
+	best_frequency = gls_b(data.x.data(), data.y.data(), data.dy.data(), length_of_data, number_of_steps, step_size, frequencies_array); //declares and fills a powers array //
 
-float powers_average = best_frequency.sum_of_powers / number_of_steps; //calculates average power for the input data
+	float powers_average = best_frequency.sum_of_powers / number_of_steps; //calculates average power for the input data
 
-std::tuple<double, float, float> output_tuple = make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(length_of_data) ));
+	std::tuple<double, float, float> output_tuple = make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(length_of_data) ));
 
 return output_tuple;
 }
