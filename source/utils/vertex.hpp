c3fc6a4a80f70a2325ef11dc962ca097d595afda
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-12 00:01:50 +0200

    Add files via upload

--- source/utils/vertex.hpp
+++ source/utils/vertex.hpp
@@ -4,13 +4,27 @@
 #include <iostream>
 
 struct output_data {
-	float power, amplitude;
-	double sum_of_powers, frequency;
+    float power = 0.0f;
+    float amplitude = 0.0f;
+    double sum_of_powers = 0.0;
+    double frequency = 0.0;
 };
 
 struct measurement {
     float x;
     float y;
 };
+
+std::pair<double, double> vertex(double x1, double y1, double x2, double y2, double x3, double y3){
+    double denom = (x1 - x2) * (x1 - x3) * (x2 - x3);
+    double A     = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom;
+    double B     = (x3*x3 * (y1 - y2) + x2*x2 * (y3 - y1) + x1*x1 * (y2 - y3)) / denom;
+    double C     = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom;
+
+    double xv = -B / (2*A);
+    double yv = C - B*B / (4*A);
+
+    return std::make_pair(xv, yv);
+}
 
 #endif
