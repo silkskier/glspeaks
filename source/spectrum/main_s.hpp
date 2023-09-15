0b1c9120459bdadec0c4cbd6a73162704d82eba5
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-16 01:21:28 +0200

    Add files via upload

--- source/spectrum/main_s.hpp
+++ source/spectrum/main_s.hpp
@@ -15,9 +15,7 @@
 #include "GLS/GLS_s.hpp"
 #include "NCU/BNCU_s.hpp"
 #include "CE/CE.hpp"
-
-using namespace std;
-
+#include "NFFT/NFFT.hpp"
 
 
 void main_spectrum(int argc, char *argv[]){
@@ -66,6 +64,7 @@ unsigned int length_of_data = data.x.size();
 gls_s(data.x.data(), data.y.data(), data.dy.data(), length_of_data, grid.freq.size(), grid.fstep, grid.freq.data(), powers);
 //bncu_s(data, grid, powers);
 //ce_s(data, grid, powers);
+//nfft_s(data, grid, powers);
 
 //        for (unsigned int i = 0; i < no_steps; i++) std::cout<< frequencies[i] <<" "<< powers[i] <<std::endl; //prints power for each frequency
 // std::cout<< std::filesystem::path(file).filesystem::path::parent_path() <<std::endl; //prints input files directory
