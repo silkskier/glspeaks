3e9845f1cd8ad45c8030be9c2a7cd516c8d151e2
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-17 18:38:57 +0200

    Add files via upload

--- source/spectrum/GLS/NFFTLS.hpp
+++ source/spectrum/GLS/NFFTLS.hpp
@@ -34,21 +34,14 @@ void nfftls_s(const star &data, grid &grid, double* p) {
     */
 
    float wsum=0, Y=0, YY=0;
-   float *ts =  (float *) malloc(n * sizeof(float)),
-         *SC =  (float *) malloc(nk * sizeof(float)),
-         *S2w =  (float *) malloc(nk * sizeof(float)),
-         *C2w =  (float *) malloc(nk * sizeof(float)),
-         *Sw =  (float *) malloc(nk * sizeof(float)),
-         *Cw =  (float *) malloc(nk * sizeof(float)),
-         *YC =  (float *) malloc(nk * sizeof(float)),
-         *YS =  (float *) malloc(nk * sizeof(float)),
-         *CC =  (float *) malloc(nk * sizeof(float)),
-         *SS =  (float *) malloc(nk * sizeof(float));
+   float *ts =  (float *) malloc(n * sizeof(float));
 
    std::complex<float> *wy = (std::complex<float> *) malloc(n * sizeof(std::complex<float>)),
                        *w =  (std::complex<float> *) malloc(n * sizeof(std::complex<float>)),
                        *Sh_Ch = (std::complex<float> *) malloc (nk * sizeof(std::complex<float>)),
                        *S2_C2 = (std::complex<float> *) malloc (nk * sizeof(std::complex<float>));
+
+   float SC, S2w, C2w, Cw, Sw, YC, YS, CC, SS;
 
 
    for (unsigned int i=0; i<n; ++i) {
@@ -69,29 +62,29 @@ void nfftls_s(const star &data, grid &grid, double* p) {
       wy[i] *= w[i].real();                /* attach weights */
    }
 
+   { //zero-mean fastsum
    fastsum(ts, wy, grid.freq.data(), Sh_Ch, n, nk);
-
    for (unsigned int i=0; i<n; ++i) {ts[i] *= 2;}
-
    fastsum(ts, w, grid.freq.data(), S2_C2, n, nk);
+   }
 
     for (unsigned int i=0; i<nk; ++i){
-        SC[i] = S2_C2[i].imag() / S2_C2[i].real();
-        S2w[i] = SC[i] / sqrt(1 + (SC[i] * SC[i]));
-        C2w[i] = 1 / sqrt(1 + (SC[i] * SC[i]));
-        Cw[i] = M_SQRT1_2 * sqrt(1 + C2w[i]);
-        Sw[i] = M_SQRT1_2 * std::copysignf(1.0, S2w[i]) * sqrt(1 - C2w[i]);
+        SC = S2_C2[i].imag() / S2_C2[i].real();
+        S2w = SC / sqrt(1 + (SC * SC));
+        C2w = 1 / sqrt(1 + (SC * SC));
+        Cw = M_SQRT1_2 * sqrt(1 + C2w);
+        Sw = M_SQRT1_2 * std::copysignf(1.0, S2w) * sqrt(1 - C2w);
 
-        YC[i] = (Sh_Ch[i].real() * Cw[i]) + (Sh_Ch[i].imag() * Sw[i]);
-        YS[i] = (Sh_Ch[i].imag() * Cw[i]) - (Sh_Ch[i].real() * Sw[i]);
+        YC = (Sh_Ch[i].real() * Cw) + (Sh_Ch[i].imag() * Sw);
+        YS = (Sh_Ch[i].imag() * Cw) - (Sh_Ch[i].real() * Sw);
 
-        CC[i] = 0.5 * ((1 + S2_C2[i].real() * S2_C2[i].real()) + (S2_C2[i].imag() * S2_C2[i].imag()));
-        SS[i] = 0.5 * ((1 + S2_C2[i].real() * S2_C2[i].real()) - (S2_C2[i].imag() * S2_C2[i].imag()));
+        CC = 0.5 * ((1 + S2_C2[i].real() * S2_C2[i].real()) + (S2_C2[i].imag() * S2_C2[i].imag()));
+        SS = 0.5 * ((1 + S2_C2[i].real() * S2_C2[i].real()) - (S2_C2[i].imag() * S2_C2[i].imag()));
 
-        p[i] =  ((YC[i] * YC[i] / CC[i]) + (YS[i] * YS[i] / SS[i])) / YY;
-    } // tan_2omega_tau
+        p[i] =  ((YC * YC / CC) + (YS * YS / SS)) / YY;
+    } //SC - tan_2omega_tau
 
-
+    free(ts); free(w); free(wy); free(Sh_Ch); free(S2_C2);
 
 
 
