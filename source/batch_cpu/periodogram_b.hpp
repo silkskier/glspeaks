639f1dc97793f9be111ebec2044263097995edf7
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-29 16:00:36 +0200

    Add files via upload

--- source/batch_cpu/periodogram_b.hpp
+++ source/batch_cpu/periodogram_b.hpp
@@ -31,6 +31,6 @@ std::tuple<float, float, float> periodogram(Grid &grid, std::filesystem::path in
 	//best_frequency = ce_b(data, grid);
 	float powers_average = best_frequency.sum_of_powers / double(grid.freq.size()); //calculates average power for the input data
 
-	std::tuple<double, float, float> output_tuple = std::make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(data.x.size()) )));
+	std::tuple<double, float, float> output_tuple = std::make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(data.x.size())));
 return output_tuple;
 }
