42b977ca04a9e648dc81ae507eddc1eddeab0dc3
silkskier <121263412+silkskier@users.noreply.github.com>
2023-09-29 18:50:18 +0200

    Update periodogram_b.hpp

--- source/batch_cpu/periodogram_b.hpp
+++ source/batch_cpu/periodogram_b.hpp
@@ -6,27 +6,19 @@
 #include <cmath>
 
 #include "GLS/GLS.hpp"
-//#include "GLS/NFFTLS.hpp" //WIP
+#include "GLS/NFFTLS.hpp" //WIP
 #include "CNU/BCNU.hpp"
 #include "CE/CE.hpp"
 
 #include "../utils/grid.hpp"
 #include "../utils/readout.hpp"
 
-/*struct output_data {
-	float power, amplitude, frequency;
-	double sum_of_powers;
-};*/
-
-
 std::tuple<float, float, float> periodogram(Grid &grid, std::filesystem::path in_file){
 
 	star data; data.read(in_file);
-
 	output_data best_frequency;
 
 	best_frequency = gls_b(data, grid); //recursive GLS
-	//best_frequency = nfftls_b(data, grid); //FFT-based GLS
 	//best_frequency = bncu_b(data, grid); //binned conditional nonuniformity, requires fixing weighting function
 	//best_frequency = ce_b(data, grid);
 	float powers_average = best_frequency.sum_of_powers / double(grid.freq.size()); //calculates average power for the input data
@@ -34,3 +26,18 @@ std::tuple<float, float, float> periodogram(Grid &grid, std::filesystem::path in
 	std::tuple<double, float, float> output_tuple = std::make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(data.x.size())));
 return output_tuple;
 }
+
+
+std::tuple<float, float, float> periodogram(Grid &grid, std::filesystem::path in_file, finufft_opts* opts){
+	//despite being polymorphic doesn't slow the execution down noticeably due to the size of periodogram functions
+
+	star data; data.read(in_file);
+	output_data best_frequency;
+	float powers_average = best_frequency.sum_of_powers / double(grid.freq.size()); //calculates average power for the input data
+
+	best_frequency = nfftls_b(data, grid, opts); //FFT-based GLS
+
+	std::tuple<double, float, float> output_tuple = std::make_tuple(best_frequency.frequency, best_frequency.amplitude, best_frequency.power / (powers_average * 2. * log2(data.x.size())));
+return output_tuple;
+}
+
